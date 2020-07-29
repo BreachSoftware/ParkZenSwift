175680c50ba7819c8a6890ebfb0e5f61e6668d90
Colin Hebert <cgabear@gmail.com>
2020-07-29 15:09:36 -0500

    Clean!

---
+++ CleanParkZen/CleanParkZen/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CleanParkZen
+//
+//  Created by Colin Hebert on 7/29/20.
+//  Copyright © 2020 Colin Hebert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
--- ParkZen/ParkZen/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  ParkZen
-//
-//  Created by Colin Hebert on 7/3/20.
-//  Copyright © 2020 Colin Hebert. All rights reserved.
-//
-
-import Foundation
